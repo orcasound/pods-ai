c6d2962157080f954da40c17400c31d1be3909b2
Dave Thaler <dthaler1968@gmail.com>
2026-05-08 09:53:00 -0700

    Add a print statement
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- src/process_false_positives.py
+++ src/process_false_positives.py
@@ -191,6 +191,8 @@ def process_false_positives(
                         summary["unknown_class"] += 1
                         continue
 
+                    print(f"Running add_samples.py for {feed.node_name} {timestamp_str} with corrected class '{corrected_class}'...")
+
                     segment_rows = add_samples(
                         wav_file=wav_path,
                         node_name=feed.node_name,
