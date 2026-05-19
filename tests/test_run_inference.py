c736d7a54030828ce0f34f98b19439adee672602
Dave Thaler <dthaler1968@gmail.com>
2026-05-18 19:40:53 -0700

    Update inferencing to use AST model
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- tests/test_run_inference.py
+++ tests/test_run_inference.py
@@ -26,7 +26,7 @@ import soundfile as sf
 # Pinned PODS-AI model revision for integration-test stability.
 PODSAI_TEST_MODEL_ID = "davethaler/whale-call-detector"
 # renovate: datasource=git-refs depName=https://huggingface.co/davethaler/whale-call-detector versioning=git.
-PODSAI_TEST_MODEL_REVISION = "cef82c6e9ee661646ea0c583aeb68f4f7ec6d9d8"
+PODSAI_TEST_MODEL_REVISION = "d1eedf5c614268da7551039a84dfc35d317168b9"
 
 
 # ---------------------------------------------------------------------------
